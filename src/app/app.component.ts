9e86162138e3f38c067bfd3e872eec7ce64def47
moeabdol <mohd.a.saed@gmail.com>
2017-09-27 10:54:08 +0300

    Simple force graph example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 
 import * as d3 from 'd3';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent implements OnInit {
   ngOnInit() {
     console.log('d3.js version:', d3['version']);
+
+    this.forceGraph();
+  }
+
+  forceGraph() {
+    const width = 800;
+    const height = 600;
+    const nodes = [{}, {}, {}, {}, {}];
+
+    const simulation = d3.forceSimulation(nodes)
+      .force('charge', d3.forceManyBody().strength(-600))
+      .force('center', d3.forceCenter(width / 2, height / 2))
+      .on('tick', ticked);
+
+    function ticked() {
+      const u = d3.select('#force-graph')
+        .selectAll('circle')
+        .data(nodes)
+        .enter()
+        .append('circle')
+        .attr('class', 'node')
+        .attr('r', 5)
+        .attr('cx', (d) => d['x'])
+        .attr('cy', (d) => d['y'])
+        .exit()
+        .remove();
+    }
   }
 }
